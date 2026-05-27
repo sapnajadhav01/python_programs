006243e74d8aad6caa2282746b1311be7fe83df8
Sapna <mj0910062@gmail.com>
2026-05-27 15:04:52 +0530

    Added remainder program

---
+++ remainder.py
@@ -0,0 +1,7 @@
+#WAP to dispplay Quotient and remainder after division
+num1=int(input("Enter First number:"))
+num2=int(input("Enter Second number:"))
+quotient=num1//num2
+remainder=num1%num2
+print("Quotient=",quotient)
+print("Remainder=",remainder)
