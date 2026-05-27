43743059d1938cd9c32892d7123aee6fa8d4c356
Sapna <mj0910062@gmail.com>
2026-05-27 15:42:44 +0530

    Added Positive program

---
+++ positive.py
@@ -0,0 +1,22 @@
+#WAP to check the given number is positive or negative
+n1=int(input("Enter A number="))
+if (n1>=0):
+    print("Positive number=",n1)
+else:
+    print("negative number")
+
+#WAp to check the number is even or odd
+n=int(input("Enter a number="))
+if(n%2==0):
+    print("Even number")
+else:
+    print("Odd")   
+
+#WAP to check The person is valide for voting
+age=int(input("Enter your age="))
+if(age>=18):
+    print("valid for the voting")
+else:
+    print("Not valid for voting")
+
+
