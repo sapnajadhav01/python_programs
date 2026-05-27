d1cd69ff04a69769e88353096d13677aebbae937
Sapna <mj0910062@gmail.com>
2026-05-27 14:56:06 +0530

    Added avg program

---
+++ avg.py
@@ -0,0 +1,26 @@
+#Total and Average of 3 numbers.
+n1=10;
+n2=15;
+n3=20;
+total=n1+n2+n3;
+print(f" total of 3 numbers={total}");
+avg=total/3;
+print(f" Average of total={avg}");
+
+#WAP to calculate time taken using speed and distance
+distance=float(input("Enter distance:"))
+speed=float(input("Enter speed:"))
+time=distance/speed
+print("time taken=",time)
+
+#WAP to calculate speed using distance and time
+distance=float(input("Enter distance="))
+time=float(input("Enter time="))
+speed=distance/time
+print("Speed taken=",speed)
+
+#WAP to calculate distance using speed and time
+distance=float(input("Enter distance="))
+speed=float(input("Enter speed="))
+time=distance/speed
+print("time taken=",time)
