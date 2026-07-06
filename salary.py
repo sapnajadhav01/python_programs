3f867d90757bb72be8fb725b92ed19c8546aba61
Sapna <mj0910062@gmail.com>
2026-07-07 00:49:59 +0530

    Added Employee Salary Calculator

---
+++ salary.py
@@ -0,0 +1,5 @@
+employee_name = input("Enter Employee Name: ")
+salary = float(input("Enter Monthly Salary: "))
+anual_salary = salary * 12
+print("Employee :", employee_name)
+print("Anual Salary :", anual_salary)
